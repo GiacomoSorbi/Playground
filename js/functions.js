21f5018175310f195efa756a82cc4cd53294bcae
Giacomo Sorbi <g.sorbi@gmail.com>
2015-09-25 13:53:39 +0100

    return key handled

--- js/functions.js
+++ js/functions.js
@@ -4,25 +4,33 @@ $(function() {
     document.getElementById("subscribe").onfocus=function(){
         document.getElementById("subscribe").parentNode.classList.add('input--filled');
         var box=document.getElementById('submessage');
-        box.hide();
+        box.style.display = 'none';
     };
     document.getElementById("subscribe").onblur=function(){
         var msgBox=document.getElementById('subscribe');
         var box=document.getElementById('submessage');
         if (msgBox.value==""){
             msgBox.parentNode.classList.remove('input--filled');
-            box.hide();
+            box.style.display = 'none';
         }
         else{
             var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
             var validMail = re.test(msgBox.value);
-            if (validMail){
-                box.className('error-email');
-                box.innerHTML('<i class="fa fa-times-circle"></i>  Please enter a valid email');
-                box.show();
+            if (!validMail){
+                box.className = 'error-email';
+                box.innerHTML = '<i class="fa fa-times-circle"></i> Please enter a valid email';
+                box.style.display = 'block';
             }
         }
     };
+
+    //suppressing the return key default behaviour
+    $('form').bind("keypress", function(e) {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            submitNewsletter();
+        }
+    });
 
     //overlay menu event listener
     $('#toggle').click(function() {
@@ -40,23 +48,22 @@ function submitNewsletter() {
     })
         .done(function (msg) {
             var box=document.getElementById('submessage');
-            box.className('confirm');
-            box.innerHTML('<i class="fa fa-check-circle"></i> Thank you for subscribing to our newsletter');
-            box.show();
+            box.className = 'confirm';
+            box.innerHTML = '<i class="fa fa-check-circle"></i> Thank you for subscribing to our newsletter';
+            box.style.display = 'block';
         })
         .fail(function( jqXHR, textStatus ) {
             var box=document.getElementById('submessage');
-            alert(JSON.parse(jqXHR.responseText).error.message);
             msg='';
             if (msg.indexOf('List_AlreadySubscribed')!=-1){
-                box.className('error-already');
-                box.innerHTML('<i class="fa fa-times-circle"></i> Your email is already in our database');
-                box.show();
+                box.className = 'error-already';
+                box.innerHTML = '<i class="fa fa-times-circle"></i> Your email is already in our database';
+                box.style.display = 'block';
             }
             else{
-                box.className('error-sent');
-                box.innerHTML('<i class="fa fa-times-circle"></i> We are sorry, but a problem as occurred; please, try again later');
-                box.show();
+                box.className = 'error-sent';
+                box.innerHTML = '<i class="fa fa-times-circle"></i> We are sorry, but a problem as occurred; please, try again later';
+                box.style.display = 'block';
             }
         });
 }
